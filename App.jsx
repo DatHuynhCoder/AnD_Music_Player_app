1b08dc931a416f7a8a4359a9752d4faf784dc13c
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2024-11-11 12:21:37 +0700

    cài expo-media-library và xin cấp quyền cho app

--- App.jsx
+++ App.jsx
@@ -7,32 +7,36 @@ import Login from './src/pages/Login';
 import Signup from './src/pages/Signup';
 import MainBottom from './src/pages/MainBottom';
 
+import { AudioProvider } from './src/context/AudioProvider';
+
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='LaunchScreen'
-      >
-        <Stack.Screen
-          name='LaunchScreen'
-          component={LaunchScreen}
-        />
-        <Stack.Screen 
-          name='Login'
-          component={Login}
-        />
-        <Stack.Screen 
-          name='Signup'
-          component={Signup}
-        />
-        <Stack.Screen 
-          name='MainBottom'
-          component={MainBottom}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AudioProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='LaunchScreen'
+        >
+          <Stack.Screen
+            name='LaunchScreen'
+            component={LaunchScreen}
+          />
+          <Stack.Screen 
+            name='Login'
+            component={Login}
+          />
+          <Stack.Screen 
+            name='Signup'
+            component={Signup}
+          />
+          <Stack.Screen 
+            name='MainBottom'
+            component={MainBottom}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AudioProvider>
   );
 }
 
